0414018db0cc97e84aa34804b5b98c69a96d5e39
secretarybird97 <81328194+secretarybird97@users.noreply.github.com>
2022-10-23 11:36:32 -0700

    Lab 321 commit

---
+++ OOP/Lab321/Célula_vegetal.cpp
@@ -0,0 +1,5 @@
+
+#include "Célula_vegetal.h"
+#include "Célula_procariota.h"
+#include "Célula_eucariota.h"
+
---
+++ OOP/Lab321/Organulos.cpp
@@ -0,0 +1,20 @@
+
+#include "Organulos.h"
+#include "CitoPlasma.h"
+
+int Organulos::getLisosomas() {
+	return this->_lisosomas;
+}
+
+void Organulos::setLisosomas(int aLisosomas) {
+	this->_lisosomas = aLisosomas;
+}
+
+int Organulos::getPlastidos() {
+	return this->_plastidos;
+}
+
+void Organulos::setPlastidos(int aPlastidos) {
+	this->_plastidos = aPlastidos;
+}
+
