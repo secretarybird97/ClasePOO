7aa31c76238b501cfe52aade78097462f29641f6
secretarybird97 <81328194+secretarybird97@users.noreply.github.com>
2022-11-28 20:39:39 -0800

    Add files via upload

---
+++ OOP/Hola_Mundo.java
@@ -0,0 +1,19 @@
+public class Hola_Mundo {
+	private String _saludo;
+
+	public String getSaludo() {
+		return this._saludo;
+	}
+
+	public void setSaludo(String aSaludo) {
+		this._saludo = aSaludo;
+	}
+
+	public string print() {
+		return "Hola mundo";
+	}
+
+	public static void main(String args[]){
+		print();
+	}
+}
